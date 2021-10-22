bc126ebd36c81ae6839e720964850926cbcb3f3c
Anna <mselnikanna@gmail.com>
2021-10-22 13:19:32 +0300

    aded task 3

---
+++ Lesson1/task3/task 3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    a = 50;
+    b = 20;
+    cout << "a= 50" << endl;
+    cout << "b= 20" << endl;
+        a =a+b;
+        b= a-b;
+        a=a-b;
+        cout << "a= " ;
+        cout << a << endl;
+        cout << "b= ";
+        cout << b << endl;
+}
+
