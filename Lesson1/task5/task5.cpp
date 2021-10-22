8a97a468580b51e346f13c3203124e7306227ea1
Anna <mselnikanna@gmail.com>
2021-10-23 00:04:24 +0300

    add task5

---
+++ Lesson1/task5/task5.cpp
@@ -0,0 +1,21 @@
+﻿// task5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int y;
+    cout << "Enter the weight of the spaceship" << endl;
+    cin >> y;
+    y = y / 3 - 2;
+    cout << "this is fuel:";
+    cout << y << endl;
+
+
+}
+
+
+
+
