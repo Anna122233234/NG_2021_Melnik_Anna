a6a3939acba844772c861133efaef9f1cef272ad
Anna <mselnikanna@gmail.com>
2021-11-01 20:32:50 +0000

    add task6

--- Lesson1/task5/task5.cpp
+++ Lesson1/task5/task5.cpp
@@ -10,9 +10,7 @@ int main()
     cout << "Enter the weight of the spaceship" << endl;
     cin >> y;
     y = y / 3 - 2;
-    cout << "this is fuel:";
-    cout << y << endl;
-
+    cout << "this is fuel:"y;
 
 }
 
--- Lesson1/task6/task 6.cpp
+++ Lesson1/task6/task 6.cpp
@@ -6,25 +6,17 @@ int main()
     int testingSwitchNumber= -1;
     cout << "Enter number: 255.255.255.*";
     cin >> testingSwitchNumber;
-
+      cout << "Number of the address:" ;
     switch (testingSwitchNumber)
     {
-    case 255:cout << "Number of the address: 1 " ;
-        break;
-    case 254:cout << "Number of the address: 2 " ;
-        break;
-    case 252:cout << "Number of the address: 4 " ;
-        break;
-    case 248:cout << "Number of the address: 8 " ;
-        break;
-    case 240:cout << "Number of the address: 16 " ;
-        break;
-    case 224:cout << "Number of the address: 32 " ;
-        break;
-    case 192:cout << "Number of the address: 64 " ;
-        break;
-    case 128:cout << "Number of the address: 128 " ;
-        break;
+    case 255:cout << " 1 " ; break;
+    case 254:cout << " 2 " ; break;
+    case 252:cout << " 4 " ; break;
+    case 248:cout << " 8 " ; break;
+    case 240:cout << " 16 " ; break;
+    case 224:cout << " 32 " ; break;
+    case 192:cout << " 64 " ; break;
+    case 128:cout << " 128 " ; break;
     default: cout << "Wrong number!!!" ;
     }
     cout << endl;
