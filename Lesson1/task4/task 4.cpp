da57ffda5d77584f780d5af3020251bc0cf833bf
Anna <mselnikanna@gmail.com>
2021-11-11 18:13:44 +0000

    updated task 2

--- Lesson1/task4/task 4.cpp
+++ Lesson1/task4/task 4.cpp
@@ -4,27 +4,27 @@
 using namespace std;
 int main()
 {
-    int a, b, c, x1, x2;
+    int a=0, b=0, c=0, x1=0, x2=0;
     cout << "Enter a " << endl;
     cin >> a ;
     cout << "Enter b " << endl;
-    cin >> b ;
+    cin >> b;
     cout << "Enter c " << endl;
-    cin >> c ;
+    cin >> c;
     int D;
     D = b * b - 4 * a * c;
     cout << " D = " << D;
-        if (D > 0) {
-            x1 = -b - sqrt(D) / 2;
-            cout << x1 << endl;
-            x2 = -b + sqrt(D) / 2;
-            cout << x2 ;
+    if (D > 0) {
+        x1 = -b - sqrt(D) / 2;
+        cout << x1 << endl;
+        x2 = -b + sqrt(D) / 2;
+        cout << x2 ;
         }
-        if (D == 0) {
-            cout << "There is no roots" << endl;
+    if (D == 0) {
+        cout << "There is no roots" << endl;
         }
-        if (D < 0) {
-            x1 = -b - sqrt(D) / 2;
-            cout << x1 ;
+    if (D < 0) {
+        x1 = -b - sqrt(D) / 2;
+        cout << x1 ;
         }
 }
