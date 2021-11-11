199577bc20cd9327217fa5883a759a1c094f5c87
Anna <mselnikanna@gmail.com>
2021-11-11 20:57:40 +0000

    update task 5

--- Lesson1/task5/task5.cpp
+++ Lesson1/task5/task5.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main()
 {
     int y = 0;
-    cout << "Enter the weight of the spaceship" << endl;
+    cout << "Enter the weight of the spaceship ";
     cin >> y;
     y = (y / 3 ) - 2;
-    cout << "this is fuel:" << y;
+    cout << "This is fuel: " << y;
     return 0;
 }
 
