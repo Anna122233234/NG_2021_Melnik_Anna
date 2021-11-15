4a3aeb1999ca995f57c181d500c0f61f36dab9dc
Anna <mselnikanna@gmail.com>
2021-11-15 22:28:15 +0000

    add task 5

---
+++ Lesson1/task5/task5/task5/task5.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    int y = 0;
+    cout << "Enter the weight of the spaceship ";
+    cin >> y;
+    cout << "This is fuel: " << (y / 3) - 2 << endl;
+    cout << "This is y: " << y;
+
+    return 0;
+}
