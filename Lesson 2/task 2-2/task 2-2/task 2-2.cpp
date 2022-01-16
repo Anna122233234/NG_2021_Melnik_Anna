7d9540b07c72d25ce1308778910a79073f60c01a
MelnikAnna_N <mselnikanna@gmail.com>
2022-01-16 21:51:56 +0200

    add task 1

---
+++ Lesson 2/task 2-2/task 2-2/task 2-2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int number = 0;
+	cout << "Enter the number\n";
+	cin >> number;
+
+	if (  int i = number; i > 0; (i % 2) = 0)
+	{
+		cout << "This is " << number;
+
+	}
+	return 0;
+}
